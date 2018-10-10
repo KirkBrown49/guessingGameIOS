47fab5c26e79b9019e02a67f1fe495936736faa5
Kirk Brown <kirkbrown@macbook-pro-97.local>
2018-10-10 10:04:24 -0500

    "first commit , just started"

--- guessingGameIOS/ViewController.swift
+++ guessingGameIOS/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var instrustionsLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var remainingLabel: UILabel!
+    
+    @IBAction func guessedButtonTapped(_ sender: Any) {
+        var randomNum = Int.random(in:1..<100)
+        let userInput = Int(readLine()!)
+        if userInput! > randomNum {
+            print("too high")
+        }else {
+            print("too low")
+        }
+        feedbackLabel.text = "You guessed \(guessTextField.text!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+ 
 }
 
